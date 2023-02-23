0e6ffe178b7e9d3d373cb8dd81824f8d1c15966a
yubn21 <125863226+yubn21@users.noreply.github.com>
2023-02-23 13:36:52 +0100

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   items: Product[] = [];
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +20,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
